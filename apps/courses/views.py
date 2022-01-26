b45be2fc74f0621d1432007c68e93921279319da
kk <705555262@qq.com>
2022-01-26 14:07:36 +0800

    add subject-course list and detail

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,7 +1,9 @@
 from django.apps import apps
+from django.db.models import Count
 from django.forms import modelform_factory
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, \
@@ -10,7 +12,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, \
                                        PermissionRequiredMixin
 
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 
 
 class OwnerMixin(object):
@@ -144,3 +146,29 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({"module": module})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses,
+        })
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
